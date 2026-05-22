41d12c54bd2a6224f24e630a62b402e4a4a37b33
jubairahmed22 <jubairahmed060@gmail.com>
2026-05-22 12:58:17 +0600

    added something

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -91,7 +91,7 @@ export default function DashboardLayout({
 
         {/* LEFT */}
         <div className="flex items-center gap-4 md:gap-8">
-asdfas
+
           {/* MOBILE MENU */}
           <button
             onClick={toggleSidebar}
