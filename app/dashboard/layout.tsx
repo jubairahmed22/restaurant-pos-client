2c0d1885aab910aa672991a1bea23632119ed42d
jubairahmed22 <jubairahmed060@gmail.com>
2026-05-22 12:57:29 +0600

    added something

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -90,7 +90,7 @@ export default function DashboardLayout({
       <header className="h-16 bg-[#6366F1] flex items-center justify-between px-4 md:px-6 sticky top-0 z-[60] shadow-md">
 
         {/* LEFT */}
-        <div className="flex items-center gap-4 md:gap-8">
+        <div className="flex items-center gap-4 md:gap-8">asdfasd
 
           {/* MOBILE MENU */}
           <button
