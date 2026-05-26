781ade0bff9c0703942f86655c55ef3d8f912df5
jubairahmed22 <jubairahmed060@gmail.com>
2026-05-26 10:22:14 +0600

    added something

--- components/menu/CheckoutPanelMenu.tsx
+++ components/menu/CheckoutPanelMenu.tsx
@@ -90,7 +90,7 @@ export default function CheckoutPanelMenu({
   };
 
   /* ───────────────── OPEN CHECKOUT ───────────────── */
-sadfsad
+
   const handleOpenCheckout = () => {
     if (cart.length === 0) return;
 
