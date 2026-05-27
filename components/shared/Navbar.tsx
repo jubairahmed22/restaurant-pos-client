61c62cc57c882c3d06ada455eb124987600fbd1a
jubairahmed22 <jubairahmed060@gmail.com>
2026-05-28 00:53:41 +0600

    added reservation perfectly

--- components/shared/Navbar.tsx
+++ components/shared/Navbar.tsx
@@ -29,7 +29,7 @@ export default function Navbar() {
   ];
 
   return (
-    <header className="fixed top-0 left-0 z-50 flex items-start">
+    <header className="fixed top-0 left-0 z-50 flex items-start">SFDGSDF
       {/* MAIN PILL 
           - relative: so we can position the scoop
           - after: the pseudo-element that creates the 'inverted' curve
