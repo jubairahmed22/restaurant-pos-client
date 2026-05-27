ffc827733da29fa3297a6d0328c30d6b76a60378
jubairahmed22 <jubairahmed060@gmail.com>
2026-05-27 16:17:13 +0600

    added about page

--- app/(main)/about/page.tsx
+++ app/(main)/about/page.tsx
@@ -166,7 +166,7 @@ const Page = () => {
         </div>
 
       </div>
-asdfasd
+
       <style jsx global>{`
         .no-scrollbar::-webkit-scrollbar { display: none; }
         .no-scrollbar { -ms-overflow-style: none; scrollbar-width: none; }
