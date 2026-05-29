552825875393e1e908447d41cd06e8bcd74d6186
jubairahmed22 <jubairahmed060@gmail.com>
2026-05-30 00:50:38 +0600

    added design perfectly

--- components/menu/CheckoutPanelMenu.tsx
+++ components/menu/CheckoutPanelMenu.tsx
@@ -11,7 +11,7 @@ import OrderItemMenu from './OrderItemMenu';
 import { OrderService } from '@/services/order.service';
 
 /* ─────────── TYPES ─────────── */
-
+sadfasd
 export interface CustomerInfo {
   fullName: string;
   email: string;
