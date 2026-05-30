25ee087683ee1967088a4df90d65bdf8ebd5128c
jubairahmed22 <jubairahmed060@gmail.com>
2026-05-30 23:31:04 +0600

    added design perfectly

--- components/pos/FoodGrid.tsx
+++ components/pos/FoodGrid.tsx
@@ -99,7 +99,7 @@ export default function FoodGrid({
             Loading foods...
           </div>
         )}
-asdfasdfa
+
         {!isLoading &&
           groupedFoods.map((group: any) => (
             <div key={group._id} className="space-y-5">
