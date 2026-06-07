8722ebfa6ef604368b3b0b78ed372f4ac239c76e
jubairahmed22 <jubairahmed060@gmail.com>
2026-06-07 23:58:21 +0600

    added reservation function for the api

--- next.config.ts
+++ next.config.ts
@@ -30,7 +30,7 @@ const nextConfig: NextConfig = {
         headers: [
           { key: 'Cache-Control', value: 'public, max-age=31536000, immutable' },
         ],
-      },
+      },asdf
       {
         // Cache optimised images
         source: '/_next/image(.*)',
