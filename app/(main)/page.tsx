b450a724f5a2bf1cf253791584019843ca1099ed
jubairahmed22 <jubairahmed060@gmail.com>
2026-06-08 00:41:57 +0600

    added reservation function for the api

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -271,7 +271,7 @@ const Page = () => {
             ))}
           </div>
         </div>
-asdfsad
+
         {/* Social Links */}
         <div className="grid grid-cols-1 gap-3 flex-shrink-0">
           <a
