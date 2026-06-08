4f764671ed77cc2de9ac705794fdc8c4cc810066
jubairahmed22 <jubairahmed060@gmail.com>
2026-06-09 01:24:48 +0600

    added reservation function for the api

--- components/menu/OrderItemMenu.tsx
+++ components/menu/OrderItemMenu.tsx
@@ -4,8 +4,8 @@ import React from 'react';
 import { Minus, Plus, Trash2 } from 'lucide-react';
 
 export default function OrderItemMenu({
-  item,
-  onIncrease,asdfsad
+  item,asdfas
+  onIncrease,
   onDecrease,
   onRemove,
 }: any) {
