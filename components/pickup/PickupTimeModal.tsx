12c9d9bd1b1058a96cb2d8d4714dba12d313c3a6
jubairahmed22 <jubairahmed060@gmail.com>
2026-06-12 01:41:12 +0600

    added something

--- components/pickup/PickupTimeModal.tsx
+++ components/pickup/PickupTimeModal.tsx
@@ -191,7 +191,7 @@ function PickupTimeModalContent({ onClose, onConfirm }: Omit<Props, 'isOpen'>) {
       isAsap:      false,
     });
     onConfirm?.();
-    onClose();
+    onClose();asdfsd
   };
   
   const skipToMenu = () => { onClose(); router.push('/menu'); };
