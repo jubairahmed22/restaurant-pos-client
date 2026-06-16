f3eee9e842062382cc4bbf8232585506da003944
jubairahmed22 <jubairahmed060@gmail.com>
2026-06-16 17:44:34 +0600

    something is added

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -17,7 +17,7 @@ import PickupTimeModal from '@/components/pickup/PickupTimeModal';
 import { usePickupStore } from '@/store/pickupStore';
 import RightPanelFooterCard from '@/components/shared/RightPanelFooterCard';
 
-sadfsdfs
+
 const slides = [
   {
     id: 1,
