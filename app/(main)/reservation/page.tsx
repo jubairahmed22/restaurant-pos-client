359da7057f4f550ef7fd9f0538e3c8db9bae379f
jubairahmed22 <jubairahmed060@gmail.com>
2026-06-21 02:12:18 +0600

    added something

--- app/(main)/reservation/page.tsx
+++ app/(main)/reservation/page.tsx
@@ -114,7 +114,7 @@ function BlogCard({ b }: { b: Blog }) {
           <span className="truncate">{getDomain(b.videoLink)}</span>
           <ExternalLink size={9} className="shrink-0 ml-auto" />
         </a>
-      )} adfadf
+      )}
       <div className="p-3.5">
         <h3 className="font-black text-white text-xs mb-1.5 leading-snug line-clamp-2">{b.title}</h3>
         {b.paragraph && (
