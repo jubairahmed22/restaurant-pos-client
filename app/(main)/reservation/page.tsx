e6d51b33129de03c940289320090d44e12c7f879
jubairahmed22 <jubairahmed060@gmail.com>
2026-06-21 02:18:56 +0600

    added something

--- app/(main)/reservation/page.tsx
+++ app/(main)/reservation/page.tsx
@@ -89,7 +89,7 @@ function BlogCard({ b }: { b: Blog }) {
             </div>
           )}
         </div>
-      )} sdsdafa
+      )}
 
       {/* YouTube embed */}
       {!b.images.length && ytId && (
