01fbdd6bfbea545745fc84108fc9416da578eb2f
jubairahmed22 <jubairahmed060@gmail.com>
2026-06-23 02:39:18 +0600

    added something

--- app/(main)/reservation/page.tsx
+++ app/(main)/reservation/page.tsx
@@ -256,7 +256,7 @@ export default function BookingPage() {
     if (!form.fullName || !form.phone || !form.people || !form.date || !form.time) {
       toast.error('Please fill in all required fields.');
       return;
-    } sdfasdfa
+    }
     try {
       setLoading(true);
       await ReservationService.createReservation({
