74afb752325e2aaf49886c8b65af4aeb129f8746
jubairahmed22 <jubairahmed060@gmail.com>
2026-06-28 02:08:27 +0600

    added something

--- app/(main)/reservation/page.tsx
+++ app/(main)/reservation/page.tsx
@@ -250,7 +250,7 @@ export default function BookingPage() {
       }
     }
     setForm(prev => ({ ...prev, [name]: value }));
-  }; sadfasfd
+  };
 
   const handleSubmit = async () => {
     if (!form.fullName || !form.phone || !form.people || !form.date || !form.time) {
